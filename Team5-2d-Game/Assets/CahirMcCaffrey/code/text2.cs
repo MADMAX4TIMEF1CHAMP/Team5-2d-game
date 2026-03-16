2587dde6535d50c305867b27d6a7fcd5144f0c2d
Cahir McCaffrey <McCaffrey-C22@ulster.ac.uk>
2026-03-16 15:36:39 +0000

    making process on the code on the ui

---
+++ Team5-2d-Game/Assets/CahirMcCaffrey/code/text2.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using System.Diagnostics;
+
+
+public class text2 : MonoBehaviour
+{
+    public TextMeshProUGUI InstructionOnHowToMoveText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        InstructionOnHowToMoveText.text = " To switch states press the space bar";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //This toggles the entire GameObject on/off
+
+            InstructionOnHowToMoveText.gameObject.SetActive(false);
+        }
+    }
+
+}
